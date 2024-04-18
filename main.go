83390348fb8430073ace485ffa3dc798810e15b4
Arran Hobson Sayers <ahobsonsayers@gmail.com>
2024-04-18 18:56:10 +0100

    Add generation task

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/ahobsonsayers/abs-goodreads/server"
 )
 
+//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen -config .oapigen.yaml schema/openapi.yaml
+
 const serverAddress = "0.0.0.0:5555"
 
 func main() {
--- tools.go
+++ tools.go
@@ -6,5 +6,3 @@ package main
 import (
 	_ "github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen"
 )
-
-//go:generate go run github.com/deepmap/oapi-codegen/v2/cmd/oapi-codegen -config .oapigen.yaml schema/openapi.yaml
