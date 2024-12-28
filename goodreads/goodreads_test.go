4e40b8aa8605b4fb1072bae754921f9831e53285
Arran Hobson Sayers <ahobsonsayers@gmail.com>
2024-12-28 01:34:58 +0000

    Revert change to goodreads test

--- goodreads/goodreads_test.go
+++ goodreads/goodreads_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/ahobsonsayers/abs-tract/goodreads"
+	"github.com/samber/lo"
 	"github.com/stretchr/testify/require"
 )
 
@@ -35,8 +36,8 @@ func TestSearchTitle(t *testing.T) {
 func TestSearchTitleAndAuthor(t *testing.T) {
 	books, err := goodreads.DefaultClient.SearchBooks(
 		context.Background(),
-		"Mike at Wrykyn",
-		nil,
+		TheHobbitTitle,
+		lo.ToPtr(TheHobbitAuthor),
 	)
 	require.NoError(t, err)
 	checkTheHobbitBookDetails(t, books[0])
